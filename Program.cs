bd6b232ec0dfff2b0f8901afcca597a8bdde50bc
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2021-01-07 12:09:00 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace objetos_como_argumentos
         {
             Produto p1 = new Produto(1, "Read dead Redemption 2", 200f);
             Produto p2 = new Produto(2,"GTA 5", 80.99f);
-            Produto p3 = new Produto(3,"Detroit Become Huuman", 120.50f);
-            Produto p4 = new Produto(4,"Rainbow six siege", 22.99f);
+            Produto p3 = new Produto(3,"Hades", 120.50f);
+            Produto p4 = new Produto(4,"Celeste", 22.99f);
 
             Carrinho carrinho = new Carrinho();
             carrinho.AdicionarProduto(p1);
